ed376e5dafe6b9cecbf4995b96d4d6ea22bb07e2
Egan Nakano <enakano@hawaii.edu>
2017-03-18 22:53:43 -1000

    Add files via upload

---
+++ OBS.java
@@ -0,0 +1,11 @@
+class OBS{{
+   private int radial;
+   public OBS{(int radial){
+   }
+   public void setOBS(int radial){
+      this.radial = radial;
+   }
+   public int getOBS(){
+      return this.radial;
+   }
+}
