228847a3e4acc665752d95ec169915ac1b59528c
RileyMiyamoto <rkam9@hawaii.edu>
2017-03-18 15:19:31 -1000

    Add files via upload

---
+++ VOR.java
@@ -0,0 +1,11 @@
+class VOR{
+   private int radial;
+   public VOR(int radial){
+   }
+   public void setVOR(int radial){
+      this.radial = radial;
+   }
+   public int getVOR(){
+      return this.radial;
+   }
+}
