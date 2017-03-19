cae6efd23d142206ba95ecf6c3c4342ff0e8a628
RileyMiyamoto <rkam9@hawaii.edu>
2017-03-18 15:30:07 -1000

    Add files via upload

---
+++ Avionics.java
@@ -0,0 +1,17 @@
+/*************
+*This object is for the
+*cockpit. It is used to 
+*store and return desired
+*radial
+**************/
+class Avionics{
+   private int radial;
+   public Avionics(int radial){
+   }
+   public void setAvionics(int radial){
+      this.radial = radial;
+   }
+   public int getAvionics(){
+      return this.radial;
+   }
+}
