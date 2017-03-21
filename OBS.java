3d2b6abfbb06a1e7849303335dc0fb23fbf6df0a
Egan Nakano <enakano@hawaii.edu>
2017-03-21 00:19:12 -1000

    added comments to OBS.java and a bit of code

--- OBS.java
+++ OBS.java
@@ -1,7 +1,18 @@
+/*
+*This object represents the OBS.
+*It is used to represent and set the omni bearing selector. 
+*It can be used to set or retrieve the OBS setting.
+*
+*/
 class OBS{{
    private int radial;
    public OBS{(int radial){
-	   setOBS(radial);
+	   if((radial <= 359) && (radial >=0)){
+		setOBS(radial);   
+	   }
+	   else{
+		   setOBS(0);
+	   }
    }
    public void setOBS(int radial){
       this.radial = radial;
